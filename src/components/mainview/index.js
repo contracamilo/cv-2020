cdde4c7aab281fc396f69eca61a9e2b21f16537f
camilo.rivera <camilo.rivera@globant.com>
2020-07-23 00:40:10 -0500

    portfolio page created

--- src/components/mainview/index.js
+++ src/components/mainview/index.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Nav from "../nav";
 import {useTranslation} from "react-i18next";
 import setText from "../../utils/i18n/textSetter";
 import {FadeIn, FadeInTop} from "../animations";
+import useObserver from '../../hooks/useIntersectionObserver';
 
 const MainView = ({id}) => {
 	const {t} = useTranslation();
 
+
+	const [observer, setElements, entries] = useObserver({
+		threshold: 0.25,
+		root: null
+	  });
+
+	  useEffect(() => {
+		entries.forEach(entry => {
+		  const box = entry.target;
+		  if (entry.isIntersecting) {
+			setTimeout(() => {
+				box.classList.add("effect");
+			}, 600);
+			//if you need put the class only once
+			//observer.unobserve(box);
+		  } else {
+			box.classList.remove("effect");
+		  }
+		});
+	  }, [entries, observer]);
+	
+	  useEffect(() => {
+		const images = document.querySelectorAll(".main-view");;
+		setElements(images);
+	  }, [setElements]);
+
 	return (
-		<section id={id} aria-labelledby="home" className="main-view">
+		<section id={id} aria-labelledby="home" className="main-view bg-img">
 			<FadeInTop><h3>{t("job", setText("HOME_JOB"))}</h3></FadeInTop>
 			<FadeIn>
 				<h1>{t("name", setText("HOME_OWNER"))}</h1>
 			</FadeIn>
-
-			
-				<Nav />
-			
+			<Nav />	
 		</section>
 	);
 };
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import PortfolioCard from '../components/portfolioCard';
+import Button from '../components/button';
+import { Link } from 'react-router-dom';
 
 const Portfolio = () => {
     return (
+        <>
         <div className="portfolio">
-            <h2>Porta</h2>
+            <nav className="portfolio__nav">
+                    <div className="portfolio__pic"></div>
+                    <h1>Portfolio</h1>
+                    <Link to="/"><Button text={'Back'} dark={true}/></Link>
+            </nav>
+            <main className="portfolio__content">
+                <section>
+                    <PortfolioCard />
+                </section>
+                <footer>last update July 2020</footer>
+            </main>
+            
         </div>
+        
+        </>
     );
 }
 
