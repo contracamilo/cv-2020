06aa9d5f8320c08caad5cfa02d89b1dfc3ba782c
camilo.rivera <camilo.rivera@globant.com>
2020-07-29 14:10:42 -0500

    big desktop fixes

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import menuItems from "./menuItems";
 import { useTranslation } from 'react-i18next';
+import { Link } from "react-router-dom";
 
 const Nav = () => {
 	const items =  menuItems(useTranslation);
 	return (
 		<nav className="navigation">
-			<ul>{items && items.map((item, index) => <li key={index} tabIndex={index}><a href={`#${item}`}>{item}</a></li>)}</ul>
+			<ul>
+				{items && items.map((item, index) => <li key={index} tabIndex={index}><a href={`#${item}`}>{item}</a></li>)}
+				<li><Link to="/portfolio">Portfolio</Link></li>
+			</ul>
 		</nav>
 	);
 };
